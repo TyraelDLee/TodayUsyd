dae4dea9bfc513243a92345ede8657faabed14a2
Tyrael Lee <lee.tyreal12138@outlook.com>
2022-09-18 01:52:08 +1000

    add form to post create account

--- src/main/resources/static/script/register.js
+++ src/main/resources/static/script/register.js
@@ -9,33 +9,44 @@
     let userEmail = document.getElementById('set-email');
     let userPassword = document.getElementById('set-password');
     let userCPassword = document.getElementById('set-confirm-password');
+    let getVerify = document.getElementById('getVerify');
+    let verify = document.getElementById('verify');
+    let create = document.getElementById('post');
 
     userName.addEventListener('input', (e)=>{
         inputEvent(e);
     });
     userName.addEventListener('blur', (e)=>{
-        blurEvent(e);
+        blurEvent(e, e.target.value === '');
     }, true);
 
     userEmail.addEventListener('input', (e)=>{
         inputEvent(e);
     });
     userEmail.addEventListener('blur', (e)=>{
-        blurEvent(e);
+        blurEvent(e, e.target.value === '');
     });
+
+    getVerify.addEventListener('click', (e)=>{
+        requestVerify(userEmail.value);
+    })
 
     userPassword.addEventListener('input', (e)=>{
         inputEvent(e);
     });
     userPassword.addEventListener('blur', (e)=>{
-        blurEvent(e);
+        blurEvent(e, e.target.value.length > 7 && e.target.value.length < 16);
     });
 
     userCPassword.addEventListener('input', (e)=>{
         inputEvent(e);
     });
     userCPassword.addEventListener('blur', (e)=>{
-        blurEvent(e);
+        blurEvent(e, e.target.value === '' || userPassword.value !== userCPassword.value);
+    });
+
+    create.addEventListener('click', (e)=>{
+        postUserInfo(userEmail.value, userName.value, userPassword.value);
     });
 
     function inputEvent(e){
@@ -46,11 +57,38 @@
         }
     }
 
-    function blurEvent(e){
-        if (e.target.value==='')
+    // function checkPassword(pass){
+    //     let char = 'qwertyuioplkjhgfdsazxcvbnm';
+    //     let num = '0123456789';
+    //     for (let i = 0; i < pass.length; i++) {
+    //         if (num.in)
+    //     }
+    // }
+
+    function blurEvent(e, condition){
+        if (condition)
             e.target.setAttribute('style', 'border-color: var(--stress_red);');
         else
             e.target.setAttribute('style', '');
+    }
+
+    function requestVerify(email){
+        fetch(`http://localhost:8085/getSecurityCode/${email}`, {
+            method:"POST",
+            credentials:"include",
+            body:null
+        });
+    }
+
+    function postUserInfo(email, uname, upass){
+        return  fetch(`http://localhost:8085/addUser?username=${uname}&usercode=${email}&userpwd=${upass}`, {
+            method:"POST",
+            credentials:"include",
+            body:null
+        }).then(r=>r.json())
+            .then(json=>{
+                console.log(json)
+            });
     }
 }();
 

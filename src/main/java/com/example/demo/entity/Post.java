d1ac30e9ad2f0bd407b77828ee3d26a0415d17c1
felix <101862278+Felix-Qi-Wang@users.noreply.github.com>
2022-09-25 15:28:22 +0800

    remove lombok @Data

--- src/main/java/com/example/demo/entity/Post.java
+++ src/main/java/com/example/demo/entity/Post.java
@@ -1,10 +1,10 @@
 package com.example.demo.entity;
 
-import lombok.Data;
+
 
 import javax.persistence.*;
+import java.util.Objects;
 
-@Data
 @Entity
 @Table(name = "post")
 @SuppressWarnings("all")
@@ -21,4 +21,65 @@ public class Post {
     private String details;
     @Column(name="numOfLikes")
     private Integer numOfLikes;
+
+    public Post(String postID, String userid, String title, String details) {
+        this.postID = postID;
+        this.userid = userid;
+        this.title = title;
+        this.details = details;
+        this.numOfLikes = 0;
+    }
+
+    public Integer getNumOfLikes() {
+        return numOfLikes;
+    }
+
+    public void setNumOfLikes(Integer numOfLikes) {
+        this.numOfLikes = numOfLikes;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(postID, post.postID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(postID, userid, title, details, numOfLikes);
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getPostID() {
+        return postID;
+    }
+
+    public void setPostID(String postID) {
+        this.postID = postID;
+    }
 }

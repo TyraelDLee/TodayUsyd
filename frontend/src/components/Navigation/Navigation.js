246309ea1bed02a8ec6d65236b502230fd885c0c
Leelzt <lzt392013@hotmail.com>
2022-09-26 23:20:41 +0800

    clear react project

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -5,24 +5,12 @@ import './Navigation.css'
 
 class Navigation extends Component {
     render() {
-      const {search_contents} = this.props;
       return (
-        <div className='header'>
-                <div className='appName'><Link style={{textDecoration:'none', color: 'black'}} to="/">LOGO</Link></div>
-                <FormGroup className="search">
-                    <i className="fa fa-search fa-1g"></i>
-                    <FormControl type="text" value={search_contents} onChange={this.handleChange} placeholder="Search"/>
-                </FormGroup>
-          </div>
+        <div>
+
+        </div>
       );
     }
 }
-
-const mapStateToProps = (state) => {
-  return {
-      userName: state.firebase.profile.userName,
-      search_contents: state.posts.search_contents,
-  }
-};
 
 export default Navigation;
--- frontend/src/components/UserProfile/UserProfile.js
+++ frontend/src/components/UserProfile/UserProfile.js
@@ -8,33 +8,7 @@ class UserProfile extends Component {
     render() {
       return (
         <div>
-          <Navigation/>
-          <Nav defaultActiveKey="/" className="flex-column">
-          <Nav.Item>
-              <Nav.Link href="/">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-              <Nav.Link href="/">Friends</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-              <Nav.Link href="/">Visited History</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-              <Nav.Link href="/">Setting</Nav.Link>
-          </Nav.Item>
-          </Nav>
-          <div className="profile-block">
-            <div className="image-name-discrition-block">
-              <Image roundedCircle="true" className="photo-block"/>
-              <div className="name-description-block">
-                <div className="name">123</div>
-                <div className="discrition">456</div>
-              </div>
-            </div>
-            <div className="posts-block">
 
-            </div>
-          </div>
         </div>
       );
     }

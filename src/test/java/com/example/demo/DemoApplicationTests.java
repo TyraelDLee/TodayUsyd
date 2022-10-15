51fee2e9596ebc2be3dcdfd379e0d6de8e855a67
whza0659 <wzha0659@uni.sydney.edu.au>
2022-10-15 20:48:03 +1100

    update unit test for userProfile and PostByUserid

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,39 +1,13 @@
-package com.example.demo;
-
-
-import com.example.demo.dao.UserProfileDao;
-import com.example.demo.entity.Post;
-import com.example.demo.entity.UserProfile;
-import com.example.demo.service.PostService;
-import com.example.demo.service.UserProfileService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ContextConfiguration(classes = DemoApplication.class)
-public class DemoApplicationTests{
-    @Autowired
-    UserProfileService userProfileService;
-
-    @Autowired
-    PostService postService;
-
-    @Test
-    public void testUserProfile(){
-        UserProfile userProfile = userProfileService.findUserProfileByUserid("id123");
-        System.out.println(userProfile);
-    }
-
-    @Test
-    public void testPostService(){
-        List<Post> post = postService.findPostByUserId("1");
-        System.out.println(post);
-    }
-}
+//package com.example.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
